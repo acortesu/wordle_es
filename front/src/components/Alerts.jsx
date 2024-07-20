93220c9932db6c6efa90e3af859384851962fc46
Alonso Cortes <alocortes.dev@gmail.com>
2024-07-20 16:53:10 -0600

    fix: makes alerts invisible after 3 seconds

--- front/src/components/Alerts.jsx
+++ front/src/components/Alerts.jsx
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 
 export function Alert({ message, type, onRestart }) {
-    if (!message) return null; // No renderizar nada si no hay mensaje
+    const [visible, setVisible] = useState(true);
+
+    useEffect(() => {
+        if (message) {
+            setVisible(true);
+            if (type === 'warning') {
+                const timer = setTimeout(() => {
+                    setVisible(false);
+                }, 3000);
+                return () => clearTimeout(timer);
+            }
+        }
+    }, [message, type]);
+
+    if (!message || !visible) return null; // No renderizar nada si no hay mensaje o no es visible
 
     let alertClass = '';
     let isPopup = false;
@@ -11,7 +25,7 @@ export function Alert({ message, type, onRestart }) {
         alertClass = 'fixed inset-0 flex items-center justify-center bg-black bg-opacity-70';
         isPopup = true;
     } else if (type === 'warning') {
-        alertClass = 'absolute top-44 left-1/2 transform -translate-x-1/2 mt-4 p-2 pr-11 pl-11 rounded bg-yellow-500 text-white md:text-sm text-[10px]';
+        alertClass = 'absolute top-44 left-1/2 transform -translate-x-1/2 mt-4 p-2 pr-11 pl-11 rounded bg-yellow-500 text-white md:text-sm text-[10px] transition-opacity duration-1000 ease-out';
     } else {
         alertClass = 'absolute top-44 left-1/2 transform -translate-x-1/2 mt-4 p-2 pr-11 pl-11 rounded bg-red-500 text-white md:text-sm text-[10px]';
     }
